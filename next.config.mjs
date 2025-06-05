ab02d54b202e44e9ffb296c2a3e0ff22fe3f05c6
jnton <84038748+jnton@users.noreply.github.com>
2025-06-05 20:32:10 +0200

    feat: add security headers to next.config.mjs for enhanced application security

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
   transpilePackages: ["framer-motion"],
   // swcMinify has been removed in Next.js 15
   images: {
