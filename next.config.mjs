c2bbc5b1a24993cd3ad98aca5bc7f6911223f7ca
jnton <84038748+jnton@users.noreply.github.com>
2025-06-05 20:34:31 +0200

    feat: add Cross-Origin security headers in next.config.mjs to enhance application security

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
           },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
         ],
       },
     ];
