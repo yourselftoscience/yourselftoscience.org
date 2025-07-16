18450893ba90e097e71d014a5e037b77b8f0d1e2
jnton <84038748+jnton@users.noreply.github.com>
2025-07-16 08:19:44 +0200

    feat: wrap GetInvolvedClientPage in Suspense for improved loading handling; update yourselftoscience.pdf

--- src/app/get-involved/page.js
+++ src/app/get-involved/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import GetInvolvedClientPage from './GetInvolvedClientPage';
 
 export const metadata = {
@@ -6,5 +7,9 @@ export const metadata = {
 };
 
 export default function GetInvolvedPage() {
-  return <GetInvolvedClientPage />;
+  return (
+    <Suspense>
+      <GetInvolvedClientPage />
+    </Suspense>
+  );
 } 
