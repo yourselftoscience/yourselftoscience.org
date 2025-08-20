f80fb65898cf985bfbabc938845d44368910b523
jnton <84038748+jnton@users.noreply.github.com>
2025-08-20 05:45:47 +0200

    fix(resource): update back link in ResourcePage to point to home instead of resources

--- src/app/resource/[slug]/page.js
+++ src/app/resource/[slug]/page.js
@@ -81,7 +81,7 @@ export default function ResourcePage({ params }) {
     <main className="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
       {/* Back to all resources link */}
       <div className="mb-8">
-        <Link href="/resources" className="text-blue-600 hover:underline flex items-center">
+        <Link href="/" className="text-blue-600 hover:underline flex items-center">
           <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
           Back to all resources
         </Link>
