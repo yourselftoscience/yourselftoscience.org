103d7ab5125f9b4a0c466fa6dd47a72b49c6d136
jnton <84038748+jnton@users.noreply.github.com>
2025-11-12 01:02:41 +0100

    fix(Footer): update copyright notice to include alternate name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -117,7 +117,7 @@ export default function Footer() {
                 AGPL-3.0 License
               </Link>.
             </p>
-            <p>&copy; {copyrightDate} Yourself To Science<sup>™</sup>. Some Rights Reserved.</p>
+            <p>&copy; {copyrightDate} Yourself To Science<sup>™</sup> (also You2Science<sup>™</sup>). Some Rights Reserved.</p>
           </div>
         </div>
       </footer>
